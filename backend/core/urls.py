b17155829f2609d3c7b570e4d278ac566ed1c214
Lincoln-developer <angufibolinc@gmail.com>
2023-02-22 23:07:21 +0300

    added user account creation

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from rest_framework import routers
+from contact import views as contact_views
 
 router = routers.DefaultRouter()
 
@@ -8,4 +9,5 @@ urlpatterns = router.urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('contact/', contact_views.ContactAPIView.as_view())
 ]
